fbe6b2464973a57c5ed01021cbe5d50f374bee7b
work <temogogitidze@redberry.ge>
2023-05-20 23:40:34 +0400

    feat(drivers): add structure for driver migration

--- database/migrations/2023_05_20_193545_create_drivers_table.php
+++ database/migrations/2023_05_20_193545_create_drivers_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -10,6 +11,12 @@ return new class extends Migration
     {
         Schema::create('drivers', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class);
+            $table->integer('year');
+            $table->string('make');
+            $table->string('model');
+            $table->string('color');
+            $table->string('license_plate');
             $table->timestamps();
         });
     }

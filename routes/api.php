f318621a68f3751c0ccdc277ef6d2868c2cbbc3a
work <temogogitidze@redberry.ge>
2023-05-21 00:15:26 +0400

    feat(service and repositories): add service and repositories

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', LoginController::class, 'submit')->name('login');
+Route::post('/login', [LoginController::class, 'submit'])->name('login');

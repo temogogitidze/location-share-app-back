288b1607802711901ae72d1bea75025c746ed20e
work <temogogitidze@redberry.ge>
2023-05-20 23:38:22 +0400

    feat(trip): add trip model and migration

--- database/migrations/2023_05_20_193732_create_trips_table.php
+++ database/migrations/2023_05_20_193732_create_trips_table.php
@@ -6,16 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::create('drivers', function (Blueprint $table) {
+        Schema::create('trips', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        Schema::dropIfExists('drivers');
+        Schema::dropIfExists('trips');
     }
 };

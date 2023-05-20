48c89e548eca9a3e4c6581725eadc2d36304027b
work <temogogitidze@redberry.ge>
2023-05-20 23:52:47 +0400

    feat(user): add drive one-to-one relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -22,5 +23,10 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function driver(): HasOne
+    {
+        return $this->hasOne(Driver::class);
+    }
 
 }

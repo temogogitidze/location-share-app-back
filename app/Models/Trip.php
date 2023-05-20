f0d745b3fd08418ba036bbdc7f3d98f5ba48a2d1
work <temogogitidze@redberry.ge>
2023-05-20 23:56:22 +0400

    feat(trip): add belongs to relationships for driver and user

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Trip extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function driver(): BelongsTo
+    {
+        return $this->belongsTo(Driver::class);
+    }
 }

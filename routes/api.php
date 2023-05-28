4ad2780f92ae9bf0ff95fd18f85562a347e61591
work <temogogitidze@redberry.ge>
2023-05-28 21:50:37 +0400

    feat(login-verify): add api endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [LoginController::class, 'submit'])->name('login');
+Route::post('/login/verify', [LoginController::class, 'verify'])->name('login.verify');
